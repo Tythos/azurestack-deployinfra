4f16e976e0a9ad7daaab391a26c78519afd42f07
Brian Kirkpatrick <code@tythos.net>
2024-02-29 20:33:10 -0800

    fixed cloudflare resource references with new domain name

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,8 @@ output "CONTAINER_ADDRESS" {
   description = "IP address assigned to container group"
   value       = azurerm_container_group.containergroup.ip_address
 }
+
+output "CF_ZONE_ID" {
+  description = "Zone ID from CloudFlare selector; since zones are selected by filter, it is useful to verify the correct zone has been returned as the first entry"
+  value       = data.cloudflare_zones.cfzones.zones[0].id
+}
--- tlarecord.tf
+++ tlarecord.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "tlarecord" {
-  zone_id = data.cloudflare_zone.cfzone.id
-  name    = "@"
+  zone_id = data.cloudflare_zones.cfzones.zones[0].id
+  name    = data.cloudflare_zones.cfzones.zones[0].name
   value   = azurerm_container_group.containergroup.ip_address
   type    = "A"
   ttl     = 3600
